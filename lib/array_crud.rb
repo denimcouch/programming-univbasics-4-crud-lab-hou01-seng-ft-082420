a640f25952bfc8beae7ea33192f8ba4b41c4830d
Alex Mata <alexmata92@ymail.com>
2020-07-29 19:49:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  party = ["warrior", "white mage", "rogue", "black mage"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   party[2]
 end
 
