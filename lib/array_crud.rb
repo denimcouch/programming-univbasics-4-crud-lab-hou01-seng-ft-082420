f4330a44aeacba837a1c97ded3ab54eb8e23a4ed
Alex Mata <alexmata92@ymail.com>
2020-07-29 19:49:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  party = ["warrior", "white mage", "rogue", "black mage"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   party[0]
 end
 
