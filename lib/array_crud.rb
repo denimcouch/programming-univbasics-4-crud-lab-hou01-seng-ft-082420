4c420094342dd26eb4371ab9dab4105dc6e49b92
Alex Mata <alexmata92@ymail.com>
2020-07-29 19:45:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,34 +7,41 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  party = ["warrior", "white mage", "rogue", "black mage"]
   party.push["red mage"]
 end
 
 def add_element_to_start_of_array(array, element)
+  party = ["warrior", "white mage", "rogue", "black mage"]
   party.unshift["paladin"]
 end
 
 def remove_element_from_end_of_array(array)
+  party = ["warrior", "white mage", "rogue", "black mage"]
   party.pop
 end
 
 def remove_element_from_start_of_array(array)
+  party = ["warrior", "white mage", "rogue", "black mage"]
   party.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array
+  party = ["warrior", "white mage", "rogue", "black mage"]
   party[2]
 end
 
 def retrieve_first_element_from_array(array)
+  party = ["warrior", "white mage", "rogue", "black mage"]
   party[0]
 end
 
 def retrieve_last_element_from_array(array)
+  party = ["warrior", "white mage", "rogue", "black mage"]
   party[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-  
+  party = ["warrior", "white mage", "rogue", "black mage"]
+  party[0] = "paladin"
 end
